5f88f7d4dd6cdc98cbc12564aba346a25a85c8fc
ES\vitaliy.martinovich <vitaliy.martinovich@effective-soft.com>
2024-10-31 17:01:48 +0300

    Quick Start, Thinking in React, useRef, useEffect, useState

---
+++ src/AppQuickStart.tsx
@@ -0,0 +1,95 @@
+import logo from './logo.svg';
+import './AppQuickStart.css';
+import {useState} from 'react';
+
+export default function AppQuickStart() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    function handleClick2() {
+        setCount(count + 3);
+    }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+              </a>
+              <MyButton />
+              <MyButton2 count={count} onClick={handleClick} />
+              <MyButton2 count={count} onClick={handleClick2} />
+              <h1>{user.name}</h1>
+              <img
+                  className="avatar"
+                  src={user.imageUrl}
+                  alt={'Photo of ' + user.name}
+                  style={{
+                      width: user.imageSize,
+                      height: user.imageSize
+                  }}
+              />
+              <ShoppingList/>
+      </header>
+    </div>
+  );
+}
+
+const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+];
+
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+};
+
+function ShoppingList() {
+    const listItems = products.map(product =>
+        <li
+            key={product.id}
+            style={{
+                color: product.isFruit ? 'magenta' : 'darkgreen'
+            }}
+        >
+            {product.title}
+        </li>
+    );
+
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
+function MyButton2({ count, onClick }: any) {
+
+    return (
+        <button onClick={onClick}>
+            Clicked {count} times
+        </button>
+    );
+}
+
+
+function MyButton() {
+    return (
+        <button>
+            I'm a button
+        </button>
+    );
+}
---
+++ src/AppThinkingInReact.js
@@ -0,0 +1,152 @@
+﻿import { useRef, useEffect, useState } from 'react';
+
+const PRODUCTS = [
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
+
+export default function AppThinkingInReact() {
+    return <FilterableProductTable products={PRODUCTS} />;
+}
+
+function FilterableProductTable({ products }) {
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+
+    return (
+        <div>
+            <SearchBar
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+                onFilterTextChange={setFilterText}
+                onInStockOnlyChange={setInStockOnly} />
+            <ProductTable
+                products={products}
+                filterText={filterText}
+                inStockOnly={inStockOnly} />
+            <Canvas />
+        </div>
+    );
+}
+
+function SearchBar({
+    filterText,
+    inStockOnly,
+    onFilterTextChange,
+    onInStockOnlyChange
+}) {
+    return (
+        <form>
+            <input
+                type="text"
+                value={filterText} placeholder="Search..."
+                onChange={(e) => onFilterTextChange(e.target.value)} />
+            <label>
+                <input
+                    type="checkbox"
+                    checked={inStockOnly}
+                    onChange={(e) => onInStockOnlyChange(e.target.checked)} />
+                {' '}
+                Only show products in stock
+            </label>
+        </form>
+    );
+}
+
+function ProductTable({ products, filterText, inStockOnly }) {
+    const rows = [];
+    let lastCategory = null;
+
+    products.forEach((product) => {
+        if (
+            product.name.toLowerCase().indexOf(
+                filterText.toLowerCase()
+            ) === -1
+        ) {
+            return;
+        }
+        if (inStockOnly && !product.stocked) {
+            return;
+        }
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category={product.category}
+                    key={product.category} />
+            );
+        }
+        rows.push(
+            <ProductRow
+                product={product}
+                key={product.name} />
+        );
+        lastCategory = product.category;
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    );
+}
+
+function ProductCategoryRow({ category }) {
+    return (
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
+    );
+}
+
+function ProductRow({ product }) {
+    const name = product.stocked ? product.name :
+        <span style={{ color: 'red' }}>
+            {product.name}
+        </span>;
+
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    );
+}
+
+function Canvas() {
+    const [color, setColor] = useState('blue');
+    const canvasRef = useRef(null);
+
+    const reDraw = (ctx) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); 
+        ctx.fillStyle = color;
+        ctx.fillRect(Math.random() * 100, Math.random() * 100, 100, 100);
+    };
+
+    useEffect(() => {
+        const ctx = canvasRef.current.getContext('2d');
+        reDraw(ctx);
+    });
+
+    const handleButtonClick = () => {
+        setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+    };
+
+    return (
+        <div>
+            <canvas ref={canvasRef} width={200} height={200} style={{ border: "1px solid #000000" }} />
+            <button onClick={handleButtonClick}>Redraw</button>
+        </div>
+    );
+};
