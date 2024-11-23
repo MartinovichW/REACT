a8242d9ba370ea9d9ebeed18875f72e1de334896
ES\vitaliy.martinovich <vitaliy.martinovich@effective-soft.com>
2024-11-23 12:44:26 +0300

    Add TransformationTab

--- src/Canvas/App.js
+++ src/Canvas/App.js
@@ -26,7 +26,7 @@ export default function AppCanvas() {
             <div className="content">
                 <div className="controls">
                     {activeTab === 'Description' && <DescriptionTab model={state.drawable.model} dispatch={dispatch} />}
-                    {activeTab === 'Transformation' && <TransformationTab />}
+                    {activeTab === 'Transformation' && <TransformationTab drawable={state.drawable} dispatch={dispatch} />}
                     {activeTab === 'Projection' && <ProjectionTab />}
                     {activeTab === 'Light' && <LightTab />}
                 </div>
@@ -38,7 +38,9 @@ export default function AppCanvas() {
 
 const graphicsReducer = (state, action) => {
     state.drawable.model.setValue(action.fieldName, action.newValue);
-    state.drawable.draw();
+    if (action.needDraw)
+        state.drawable.draw();
+
     return { ...state, drawable: state.drawable };
 };
 
@@ -46,46 +48,123 @@ const DescriptionTab = ({ model, dispatch }) => {
     return (
         <>
             <div className="image-container" />
-            <div className="stepper-container">
-                <Stepper field='A' value={model.A} dispatch={dispatch} min={model.minA} max={12} />
-                <Stepper field='B' value={model.B} dispatch={dispatch} min={model.minB} max={12} />
-                <Stepper field='C' value={model.C} dispatch={dispatch} min={4} max={12} />
-                <Stepper field='R' value={model.R} dispatch={dispatch} min={1} max={model.maxR} />
-                <Stepper field='dX' value={model.dX} dispatch={dispatch} min={model.mindX} max={model.maxdX} />
-                <Stepper field='dZ' value={model.dZ} dispatch={dispatch} min={model.mindZ} max={model.maxdZ} />
-                <Stepper field='N' value={model.N} dispatch={dispatch} min={4} max={72} step={4} />
-            </div>
+            <table className="table">
+                <tr>
+                    <Stepper field='A' value={model.A} dispatch={dispatch} min={model.minA} max={12} />
+                    <Stepper field='B' value={model.B} dispatch={dispatch} min={model.minB} max={12} />
+                    <Stepper field='C' value={model.C} dispatch={dispatch} min={4} max={12} />
+                    <Stepper field='R' value={model.R} dispatch={dispatch} min={1} max={model.maxR} />
+                    <Stepper field='dX' value={model.dX} dispatch={dispatch} min={model.mindX} max={model.maxdX} />
+                    <Stepper field='dZ' value={model.dZ} dispatch={dispatch} min={model.mindZ} max={model.maxdZ} />
+                    <Stepper field='N' value={model.N} dispatch={dispatch} min={4} max={72} step={4} />
+                </tr>
+            </table>
         </>
     );
 };
 
-const Stepper = ({ field, value, dispatch, min, max, step = 1 }) => {
+const Stepper = ({ field, value, dispatch, min, max, step = 1, needDraw = true,
+                   handleExtraAction1, handleExtraAction2, extraActionClassName1, extraActionClassName2 }) => {
     const handleDecrement = () => {
         if (value - step >= min) {
-            dispatch({ fieldName: field, newValue: value - step });
+            dispatch({ fieldName: field, newValue: value - step, needDraw: needDraw });
         }
     };
 
     const handleIncrement = () => {
         if (value + step <= max) {
-            dispatch({ fieldName: field, newValue: value + step });
+            dispatch({ fieldName: field, newValue: value + step, needDraw: needDraw });
         }
     };
 
     return (
-        <div className="stepper-item">
+        <td className="td">
             <label>{field} = {value}</label>
             <div>
                 <button onClick={handleDecrement}>-</button>
                 <button onClick={handleIncrement}>+</button>
             </div>
-        </div>
+            <div>
+                {handleExtraAction1 && (<button className={extraActionClassName1} onClick={handleExtraAction1} />)}
+                {handleExtraAction2 && (<button className={extraActionClassName2} onClick={handleExtraAction2} />)}
+            </div>
+        </td>
     );
 };
 
-const TransformationTab = () => (
-    <button>2</button>
-);
+const TransformationTab = ({ drawable, dispatch }) => {
+    const handleExtraAction = (code) => {
+        drawable.model.calculateTransformation(code);
+        drawable.draw();
+    };
+
+    return (
+        <>
+            <table className="table">
+                <tr className="tr">
+                    <th colspan="3">Move</th>
+                </tr>
+                <tr>
+                    <Stepper field='MX' value={drawable.model.MX} dispatch={dispatch} min={1} max={3} needDraw={false}
+                        handleExtraAction1={() => handleExtraAction("11")}
+                        handleExtraAction2={() => handleExtraAction("12")}
+                        extraActionClassName1="btn dxleft"
+                        extraActionClassName2="btn dxright" />
+                    <Stepper field='MY' value={drawable.model.MY} dispatch={dispatch} min={1} max={3} needDraw={false}
+                        handleExtraAction1={() => handleExtraAction("13")}
+                        handleExtraAction2={() => handleExtraAction("14")}
+                        extraActionClassName1="btn dybuttom"
+                        extraActionClassName2="btn dytop" />
+                    <Stepper field='MZ' value={drawable.model.MZ} dispatch={dispatch} min={1} max={3} needDraw={false}
+                        handleExtraAction1={() => handleExtraAction("15")}
+                        handleExtraAction2={() => handleExtraAction("16")}
+                        extraActionClassName1="btn dzleft"
+                        extraActionClassName2="btn dzright" />
+                </tr>
+            </table>
+            <div className="table-spacing" />
+            <table className="table">
+                <tr className="tr">
+                    <th className="th" colspan="3">Scale</th>
+                </tr>
+                <tr>
+                    <Stepper field='SX' value={drawable.model.SX} dispatch={dispatch} min={0.5} max={2.0} step={0.5} needDraw={false}
+                        handleExtraAction1={() => handleExtraAction("21")}
+                        extraActionClassName1="btn sx" />
+                    <Stepper field='SY' value={drawable.model.SY} dispatch={dispatch} min={0.5} max={2.0} step={0.5} needDraw={false}
+                        handleExtraAction1={() => handleExtraAction("22")}
+                        extraActionClassName1="btn sy" />
+                    <Stepper field='SZ' value={drawable.model.SZ} dispatch={dispatch} min={0.5} max={2.0} step={0.5} needDraw={false}
+                        handleExtraAction1={() => handleExtraAction("23")}
+                        extraActionClassName1="btn sz" />
+                </tr>
+            </table>
+            <div className="table-spacing" />
+            <table className="table">
+                <tr className="tr">
+                    <th className="th" colspan="3">Rotate</th>
+                </tr>
+                <tr>
+                    <Stepper field='AX' value={drawable.model.AX} dispatch={dispatch} min={5} max={180} step={5} needDraw={false}
+                        handleExtraAction1={() => handleExtraAction("31")}
+                        handleExtraAction2={() => handleExtraAction("32")}
+                        extraActionClassName1="btn axL"
+                        extraActionClassName2="btn axR" />
+                    <Stepper field='AY' value={drawable.model.AY} dispatch={dispatch} min={5} max={180} step={5} needDraw={false}
+                        handleExtraAction1={() => handleExtraAction("33")}
+                        handleExtraAction2={() => handleExtraAction("34")}
+                        extraActionClassName1="btn ayL"
+                        extraActionClassName2="btn ayR" />
+                    <Stepper field='AZ' value={drawable.model.AZ} dispatch={dispatch} min={5} max={180} step={5} needDraw={false}
+                        handleExtraAction1={() => handleExtraAction("35")}
+                        handleExtraAction2={() => handleExtraAction("36")}
+                        extraActionClassName1="btn azL"
+                        extraActionClassName2="btn azR" />
+                </tr>
+            </table>
+        </>
+    );
+};
 
 const ProjectionTab = () => (
     <button>3</button>
--- src/Canvas/BusinessObjects/Model.js
+++ src/Canvas/BusinessObjects/Model.js
@@ -9,6 +9,13 @@ export default class Model {
         this.S = 30.0;
         this.PI = -1.0 * Math.PI / 180.0;
         this.currentProjection = "Z";
+        this.LightMode = "Off";
+        this.DX = 0.0;
+        this.DY = 0.0;
+        this.LX = 0.0;
+        this.LY = 0.0;
+        this.LZ = 0.0;
+
         this.A = 8.0;
         this.minA = 7.0;
         this.B = 8.0;
@@ -23,13 +30,21 @@ export default class Model {
         this.mindZ = 3.0;
         this.maxdZ = 5.0;
         this.N = 12;
-        this.DX = 0.0;
-        this.DY = 0.0;
-        this.LX = 0.0;
-        this.LY = 0.0;
-        this.LZ = 0.0;
-        this.S = 30;
-        this.LightMode = "Off";
+
+        this.MX = 1.0;
+        this.MY = 1.0;
+        this.MZ = 1.0;
+        this.SX = 0.5;
+        this.SY = 0.5;
+        this.SZ = 0.5;
+        this.AX = 5.0;
+        this.AY = 5.0;
+        this.AZ = 5.0;
+
+        this.PSI = 30.0;
+        this.FI = 30.0;
+        this.Alpha = 45.0;
+        this.L = 1.0;
     }
 
     get countPoints() {
@@ -91,6 +106,33 @@ export default class Model {
                 this.N = value;
                 this.calculatePoints();
                 break;
+            case 'MX':
+                this.MX = value;
+                break;
+            case 'MY':
+                this.MY = value;
+                break;
+            case 'MZ':
+                this.MZ = value;
+                break;
+            case 'SX':
+                this.SX = value;
+                break;
+            case 'SY':
+                this.SY = value;
+                break;
+            case 'SZ':
+                this.SZ = value;
+                break;
+            case 'AX':
+                this.AX = value;
+                break;
+            case 'AY':
+                this.AY = value;
+                break;
+            case 'AZ':
+                this.AZ = value;
+                break;
             default:
                 break;
         }
@@ -128,14 +170,37 @@ export default class Model {
         let y = this.DY - (this.C * this.S);
         let z = this.B * this.S;
 
-        this.originalPoints[0] = { X: this.DX, Y: this.DY, Z: 0 };
-        this.originalPoints[1] = { X: this.DX, Y: y, Z: 0 };
-        this.originalPoints[2] = { X: x, Y: y, Z: 0 };
-        this.originalPoints[3] = { X: x, Y: this.DY, Z: 0 };
-        this.originalPoints[4] = { X: x, Y: this.DY, Z: z };
-        this.originalPoints[5] = { X: x, Y: y, Z: z };
-        this.originalPoints[6] = { X: this.DX, Y: y, Z: z };
-        this.originalPoints[7] = { X: this.DX, Y: this.DY, Z: z };
+        this.originalPoints[0].X = this.DX;
+        this.originalPoints[0].Y = this.DY;
+        this.originalPoints[0].Z = 0;
+
+        this.originalPoints[1].X = this.DX;
+        this.originalPoints[1].Y = y;
+        this.originalPoints[1].Z = 0;
+
+        this.originalPoints[2].X = x;
+        this.originalPoints[2].Y = y;
+        this.originalPoints[2].Z = 0;
+
+        this.originalPoints[3].X = x;
+        this.originalPoints[3].Y = this.DY;
+        this.originalPoints[3].Z = 0;
+
+        this.originalPoints[4].X = x;
+        this.originalPoints[4].Y = this.DY;
+        this.originalPoints[4].Z = z;
+
+        this.originalPoints[5].X = x;
+        this.originalPoints[5].Y = y;
+        this.originalPoints[5].Z = z;
+
+        this.originalPoints[6].X = this.DX;
+        this.originalPoints[6].Y = y;
+        this.originalPoints[6].Z = z;
+
+        this.originalPoints[7].X = this.DX;
+        this.originalPoints[7].Y = this.DY;
+        this.originalPoints[7].Z = z;
 
         let inc = 360 / this.N, alpha1, alpha2 = inc;
 
@@ -145,12 +210,295 @@ export default class Model {
             y = this.DY - (this.C * this.S);
             z = (this.R * Math.sin(alpha1) + this.dZ) * this.S;
 
-            this.originalPoints[i] = { X: x, Y: this.DY, Z: z };
-            this.originalPoints[i + this.N] = { X: x, Y: y, Z: z };
+            this.originalPoints[i].X = x;
+            this.originalPoints[i].Y = this.DY;
+            this.originalPoints[i].Z = z;
+
+            this.originalPoints[i + this.N].X = x;
+            this.originalPoints[i + this.N].Y = y;
+            this.originalPoints[i + this.N].Z = z;
 
             alpha2 += inc;
         }
 
-        // CalculateProjection(CurrentProjection);
+        this.calculateProjection(this.currentProjection);
+    }
+
+    calculateTransformation(command) {
+        switch (command) {
+            case "11":
+                this.executeTransformation(this.move, { dsax: this.MX * -1 * this.S });
+                break;
+            case "12":
+                this.executeTransformation(this.move, { dsax: this.MX * this.S });
+                break;
+            case "13":
+                this.executeTransformation(this.move, { dsax: 0, dsay: this.MY * -1 * this.S });
+                break;
+            case "14":
+                this.executeTransformation(this.move, { dsax: 0, dsay: this.MY * this.S });
+                break;
+            case "15":
+                this.executeTransformation(this.move, { dsax: 0, dsay: 0, dsaz: this.MZ * -1 * this.S });
+                break;
+            case "16":
+                this.executeTransformation(this.move, { dsax: 0, dsay: 0, dsaz: this.MZ * this.S });
+                break;
+            case "21":
+                this.executeTransformation(this.scale, { dsax: this.SX, dsay: 1, dsaz: 1, dX: this.originalPoints[0].X * (1 - this.SX) });
+                break;
+            case "22":
+                this.executeTransformation(this.scale, { dsax: 1, dsay: this.SY, dsaz: 1, dY: this.originalPoints[0].Y * (1 - this.SY) });
+                break;
+            case "23":
+                this.executeTransformation(this.scale, { dsax: 1, dsay: 1, dsaz: this.SZ, dZ: this.originalPoints[0].Z * (1 - this.SZ) });
+                break;
+            case "31":
+                this.executeTransformation(this.rotate, {
+                    dsax: this.AX * -1,
+                    dY: this.originalPoints[0].Y * (1 - Math.cos(this.AX * -1 * this.PI)) +
+                        this.originalPoints[0].Z * Math.sin(this.AX * -1 * this.PI),
+                    dZ: this.originalPoints[0].Z * (1 - Math.cos(this.AX * -1 * this.PI)) -
+                        this.originalPoints[0].Y * Math.sin(this.AX * -1 * this.PI)
+                });
+                break;
+            case "32":
+                this.executeTransformation(this.rotate, {
+                    dsax: this.AX,
+                    dY: this.originalPoints[0].Y * (1 - Math.cos(this.AX * this.PI)) +
+                        this.originalPoints[0].Z * Math.sin(this.AX * this.PI),
+                    dZ: this.originalPoints[0].Z * (1 - Math.cos(this.AX * this.PI)) -
+                        this.originalPoints[0].Y * Math.sin(this.AX * this.PI)
+                });
+                break;
+            case "33":
+                this.executeTransformation(this.rotate, {
+                    dsay: this.AY * -1,
+                    dX: this.originalPoints[0].X * (1 - Math.cos(this.AY * -1 * this.PI)) +
+                        this.originalPoints[0].Z * Math.sin(this.AY * -1 * this.PI),
+                    dZ: this.originalPoints[0].Z * (1 - Math.cos(this.AY * -1 * this.PI)) -
+                        this.originalPoints[0].X * Math.sin(this.AY * -1 * this.PI)
+                });
+                break;
+            case "34":
+                this.executeTransformation(this.rotate, {
+                    dsay: this.AY,
+                    dX: this.originalPoints[0].X * (1 - Math.cos(this.AY * this.PI)) +
+                        this.originalPoints[0].Z * Math.sin(this.AY * this.PI),
+                    dZ: this.originalPoints[0].Z * (1 - Math.cos(this.AY * this.PI)) -
+                        this.originalPoints[0].X * Math.sin(this.AY * this.PI)
+                });
+                break;
+            case "35":
+                this.executeTransformation(this.rotate, {
+                    dsaz: this.AZ * -1,
+                    dX: this.originalPoints[0].X * (1 - Math.cos(this.AZ * -1 * this.PI)) +
+                        this.originalPoints[0].Y * Math.sin(this.AZ * -1 * this.PI),
+                    dY: this.originalPoints[0].Y * (1 - Math.cos(this.AZ * -1 * this.PI)) -
+                        this.originalPoints[0].X * Math.sin(this.AZ * -1 * this.PI)
+                });
+                break;
+            case "36":
+                this.executeTransformation(this.rotate, {
+                    dsaz: this.AZ,
+                    dX: this.originalPoints[0].X * (1 - Math.cos(this.AZ * this.PI)) +
+                        this.originalPoints[0].Y * Math.sin(this.AZ * this.PI),
+                    dY: this.originalPoints[0].Y * (1 - Math.cos(this.AZ * this.PI)) -
+                        this.originalPoints[0].X * Math.sin(this.AZ * this.PI)
+                });
+                break;
+            default:
+                break;
+        }
+
+        this.calculateProjection(this.currentProjection);
+    }
+
+    executeTransformation(transformationMethod, { dsax = 0, dsay = 0, dsaz = 0, dX = 0, dY = 0, dZ = 0 } = { }) {
+        for (let point of this.originalPoints.slice(0, this.countPoints)) {
+            const temp = transformationMethod(point, dsax, dsay, dsaz);
+
+            point.X = temp[0] + dX;
+            point.Y = temp[1] + dY;
+            point.Z = temp[2] + dZ;
+        }
+    }
+
+    move(p, dx, dy, dz) {
+        const t = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [dx, dy, dz, 1]
+        ];
+
+        const pt = [
+            (p.X * t[0][0]) + (p.Y * t[1][0]) + (p.Z * t[2][0]) + (p.A * t[3][0]),
+            (p.X * t[0][1]) + (p.Y * t[1][1]) + (p.Z * t[2][1]) - (p.A * t[3][1]),
+            (p.X * t[0][2]) + (p.Y * t[1][2]) + (p.Z * t[2][2]) + (p.A * t[3][2]),
+            (p.X * t[0][3]) + (p.Y * t[1][3]) + (p.Z * t[2][3]) + (p.A * t[3][3])
+        ];
+
+        return pt;
+    }
+
+    scale(p, sx, sy, sz) {
+        const s = [
+            [sx, 0, 0, 0],
+            [0, sy, 0, 0],
+            [0, 0, sz, 0],
+            [0, 0, 0, 1]
+        ];
+
+        const ps = [
+            (p.X * s[0][0]) + (p.Y * s[1][0]) + (p.Z * s[2][0]) + (p.A * s[3][0]),
+            (p.X * s[0][1]) + (p.Y * s[1][1]) + (p.Z * s[2][1]) + (p.A * s[3][1]),
+            (p.X * s[0][2]) + (p.Y * s[1][2]) + (p.Z * s[2][2]) + (p.A * s[3][2]),
+            (p.X * s[0][3]) + (p.Y * s[1][3]) + (p.Z * s[2][3]) + (p.A * s[3][3])
+        ];
+
+        return ps;
+    }
+
+    rotate(p, ax, ay, az) {
+        const PI = -1.0 * Math.PI / 180.0;
+
+        if (ax !== 0) {
+            const rx = [
+                [1, 0, 0, 0],
+                [0, Math.cos(ax * PI), Math.sin(ax * PI), 0],
+                [0, -Math.sin(ax * PI), Math.cos(ax * PI), 0],
+                [0, 0, 0, 1]
+            ];
+
+            const pr = [
+                (p.X * rx[0][0]) + (p.Y * rx[1][0]) + (p.Z * rx[2][0]) - (p.A * rx[3][0]),
+                (p.X * rx[0][1]) + (p.Y * rx[1][1]) + (p.Z * rx[2][1]) + (p.A * rx[3][1]),
+                (p.X * rx[0][2]) + (p.Y * rx[1][2]) + (p.Z * rx[2][2]) + (p.A * rx[3][2]),
+                (p.X * rx[0][3]) + (p.Y * rx[1][3]) + (p.Z * rx[2][3]) + (p.A * rx[3][3])
+            ];
+
+            return pr;
+        }
+
+        if (ay !== 0) {
+            const ry = [
+                [Math.cos(ay * PI), 0, Math.sin(ay * PI), 0],
+                [0, 1, 0, 0],
+                [-Math.sin(ay * PI), 0, Math.cos(ay * PI), 0],
+                [0, 0, 0, 1]
+            ];
+
+            const pr = [
+                (p.X * ry[0][0]) + (p.Y * ry[1][0]) + (p.Z * ry[2][0]) - (p.A * ry[3][0]),
+                (p.X * ry[0][1]) + (p.Y * ry[1][1]) + (p.Z * ry[2][1]) + (p.A * ry[3][1]),
+                (p.X * ry[0][2]) + (p.Y * ry[1][2]) + (p.Z * ry[2][2]) + (p.A * ry[3][2]),
+                (p.X * ry[0][3]) + (p.Y * ry[1][3]) + (p.Z * ry[2][3]) + (p.A * ry[3][3])
+            ];
+
+            return pr;
+        }
+
+        if (az !== 0) {
+            const rz = [
+                [Math.cos(az * PI), Math.sin(az * PI), 0, 0],
+                [-Math.sin(az * PI), Math.cos(az * PI), 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]
+            ];
+
+            const pr = [
+                (p.X * rz[0][0]) + (p.Y * rz[1][0]) + (p.Z * rz[2][0]) - (p.A * rz[3][0]),
+                (p.X * rz[0][1]) + (p.Y * rz[1][1]) + (p.Z * rz[2][1]) + (p.A * rz[3][1]),
+                (p.X * rz[0][2]) + (p.Y * rz[1][2]) + (p.Z * rz[2][2]) + (p.A * rz[3][2]),
+                (p.X * rz[0][3]) + (p.Y * rz[1][3]) + (p.Z * rz[2][3]) + (p.A * rz[3][3])
+            ];
+
+            return pr;
+        }
+
+        return [];
+    }
+
+    calculateProjection(projection) {
+        let px, py, pa, po;
+
+        switch (projection) {
+            case "X":
+                px = [
+                    [0, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, -1, 0],
+                    [0, 0, 0, 1]
+                ];
+
+                this.projectionMethod(px, projection, this.DX);
+                break;
+
+            case "Y":
+                py = [
+                    [1, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, -1, 0],
+                    [0, 0, 0, 1]
+                ];
+
+                this.projectionMethod(py, projection, this.DY);
+                break;
+
+            case "A":
+                pa = [
+                    [Math.cos(this.PSI * this.PI), Math.sin(this.FI * this.PI * -1) * Math.sin(this.PSI * this.PI), 0, 0],
+                    [0, Math.cos(this.FI * this.PI * -1), 0, 0],
+                    [Math.sin(this.PSI * this.PI), -Math.sin(this.FI * this.PI * -1) * Math.cos(this.PSI * this.PI), 0, 0],
+                    [0, 0, 0, 1]
+                ];
+
+                let dX = this.originalPoints[0].X * (1 - Math.cos(this.PSI * this.PI)) + this.originalPoints[0].Z * Math.sin(this.PSI * this.PI);
+                let dY = this.originalPoints[0].Z * (1 - Math.cos(this.PSI * this.PI) * Math.sin(this.FI * this.PI * -1)) - (this.originalPoints[0].X * Math.sin(this.PSI * this.PI) * Math.sin(this.FI * this.PI * -1)) + (this.originalPoints[0].Y * (1 - Math.cos(this.FI * this.PI * -1)));
+
+                this.projectionMethod(pa, projection, dX, dY);
+                break;
+
+            case "O":
+                po = [
+                    [1, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [this.L * Math.cos(this.Alpha * this.PI), this.L * Math.sin(this.Alpha * this.PI), 0, 0],
+                    [0, 0, 0, 1]
+                ];
+
+                this.projectionMethod(po, projection);
+                break;
+            default:
+                break;
+        }
+
+        this.currentProjection = projection;
+    }
+
+    projectionMethod(matrix, projection, dX = 0, dY = 0) {
+        this.refreshProjectionPoints();
+
+        for (let p of this.projectionPoints.slice(0, this.countPoints)) {
+            let pm = [
+                (p.X * matrix[0][0]) + (p.Y * matrix[1][0]) + (p.Z * matrix[2][0]) + (p.A * matrix[3][0]),
+                (p.X * matrix[0][1]) + (p.Y * matrix[1][1]) + (p.Z * matrix[2][1]) + (p.A * matrix[3][1]),
+                (p.X * matrix[0][2]) + (p.Y * matrix[1][2]) + (p.Z * matrix[2][2]) + (p.A * matrix[3][2]),
+                (p.X * matrix[0][3]) + (p.Y * matrix[1][3]) + (p.Z * matrix[2][3]) + (p.A * matrix[3][3])
+            ];
+
+            p.Z = projection === "X" ? p.X : projection === "Y" ? p.Y : p.Z;
+            p.Y = projection === "Y" ? pm[2] + dY : pm[1] + dY;
+            p.X = projection === "X" ? pm[2] + dX : pm[0] + dX;
+        }
+    }
+
+    refreshProjectionPoints() {
+        for (let [orig, proj] of this.allPoints.slice(0, this.countPoints)) {
+            proj.X = orig.X;
+            proj.Y = orig.Y;
+            proj.Z = orig.Z;
+        }
     }
 }
